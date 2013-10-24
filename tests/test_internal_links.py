ed767c64bc02cb2701d2aefb437a0528e0ad199a
Eric Matthes <ehmatthes@gmail.com>
2013-10-24 07:21:47 -0800

    Works well when SimpleHTTPServer is already running from outside the script.

--- tests/test_internal_links.py
+++ tests/test_internal_links.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import os
 
 # This test runs through all html files in /notebooks,
 #  and verifies that internal links are working.
@@ -20,21 +21,29 @@ import re
 #    Need to be serving files locally first.
 #    Start a simple server from this test to do so.
 #    Lower priority; if working deployed, should work locally.
+#    Local is default behavior, flag for deployed?
+#      Separate script for deployed?
 #  Should have option to test deployed files.
 #    Separate test, or flag for this test?
 #  Make sure file name accurately reflects what's being tested.
 
+# Start a server locally, in the notebooks directory.
+root_dir = '/srv/projects/intro_programming/intro_programming/notebooks/'
+#os.chdir(root_dir)
+# This could be improved by making sure port 8000 available.
+#os.system('python -m SimpleHTTPServer 8000 &')
+#server_pid = os.system('echo $$')
+#print("Starting server with pid: ", server_pid)
 
-filename = '/srv/projects/intro_programming/intro_programming/notebooks/index.html'
-f = open(filename, 'r')
+filename = 'index.html'
+f = open(root_dir + filename, 'r')
 lines = f.readlines()
 f.close()
+
 
 def get_links(line):
     # Returns a list of links contained in a line of code
     links = []
-    link_re = '.*a href.*'
-    link_re = '(.*a href=")(.*)(".*)'
     link_re = '=(")(.*)(".*)'
     p = re.compile(link_re)
 
@@ -48,28 +57,36 @@ def get_links(line):
 
     return links
 
+
 # Store all links to check.
 links_to_check = []
 for line in lines:
     if 'a href' in line:
-        #print line
         for link in get_links(line):
             # Ignore anchor tags for now
             if 'html' in link:
                 links_to_check.append(link)
 
-print 'Links:'
+
 # Report on progress as test runs, but store bad links for final report.
 #  dict: {link: status_code}
 bad_links = {}
+print "links to check: ", links_to_check
 for link in links_to_check:
     print "Checking link: %s..." % link
-    root = 'file:///srv/projects/intro_programming/intro_programming/notebooks/'
-    root = 'http://introtopython.org/'
-    r = requests.get(root + link)
+    #root = 'http://introtopython.org/'
+    root = 'http://localhost:8000/'
+    url = root + link
+    print 'checking url: ', url
+    r = requests.get(url)
     print 'Status code: ', r.status_code
     if r.status_code != 200:
         bad_links[link] = r.status_code
+
+
+# Kill the server process
+#kill_server_command = 'kill -9 ' + str(server_pid)
+#os.system(kill_server_command)
 
 # Report on bad links.
 print "\n\n*** Bad Links ***"
@@ -78,3 +95,4 @@ if bad_links:
         print bad_links[link], link
 else:
     print "Congratulations, all links are working."
+print "\n"
