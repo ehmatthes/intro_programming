cc7511bc13079b8ea4b80956de46b1bf69ae0491
Eric Matthes <ehmatthes@gmail.com>
2013-12-20 15:11:43 -0900

    Simplified build_html_pages.sh; prefix rather than repetitive calls to scripts.

--- scripts/build_html_pages.sh
+++ scripts/build_html_pages.sh
@@ -21,173 +21,104 @@
 
 
 if [ -e scripts/ ]
-
 then
-    # Probably running from a post-commit hook.
-
-    # Build basic pages.
-    source scripts/create_common_html.sh
-    wait
-
-	 # Create empty all_exercises_challenges page.
-	 echo "Creating empty all_exercises_challenges.html file..."
-	 touch notebooks/all_exercises_challenges.html
-	 # Next scripts require a non-empty file at this point.
-	 echo "<br>" > notebooks/all_exercises_challenges.html
-    echo "Created empty all_exercises_challenges.html file."
-	 wait
-
-    # Add opening tags
-    source scripts/add_opening_tags.sh
-    wait
-    # Add title tag
-    source scripts/add_title.sh
-    wait
-    # Add css and js links, and favicon link
-    source scripts/add_css_js_links.sh
-    wait
-    # Close head and open body
-    source scripts/close_head_open_body.sh
-    wait
-    # Close body and html tags.
-    source scripts/close_body_html.sh
-    # Add bootstrap.
-    source scripts/add_bootstrap.sh
-    wait
-
-	 # If custom index file exists, overwrite index just created.
-	 source scripts/copy_custom_index.sh
-	 wait
-
-	 # Customize all pages to use bootstrap
-     #  Also adds navbar from index to all pages
-	 printf "Customizing all styles to use bootstrap...\n"
-	 python scripts/add_bootstrap.py
-	 wait
-	 printf "Customized styles.\n\n"
-
-	 # Add elements to toggle output on each page.
-	 printf "Adding ability to toggle output on each page...\n"
-	 python scripts/show_hide_output.py
-	 wait
-	 printf "Added toggling ability.\n\n"
-
-	 # Convert index.html links to ./
-    source scripts/convert_home_links.sh
-    wait
-
-    # Add fb button.
-    source scripts/add_facebook.sh
-    wait
-    python scripts/modify_facebook_urls.py
-    wait
-
-	 # Convert image links from ipynb format to html.
-	 source scripts/convert_image_links.sh
-	 wait
-
-	 # Highlight lines of code.
-	 python scripts/highlight_code.py
-	 wait
-
-	 # Strip input references from code cells.
-	 python scripts/remove_input_references.py
-	 wait
-
-	 # Create page containing all exercises and challenges.
-	 python scripts/build_all_exercises_page.py
-	 wait
-
-    # Insert Google Analytics code.
-	 #  This should happen last, so that ga code is just before
-	 #  closing head tag.
-    source scripts/insert_google_analytics.sh
-	 wait
-
+	 # Probably running as a commit hook.
+    prefix="scripts/"
 else
-    # Probably running this script directly, from scripts/ dir.
-
-    # Build basic pages.
-    source create_common_html.sh
-    wait
-
-	 # Create empty all_exercises_challenges page.
-	 echo "Creating empty all_exercises_challenges.html file..."
-	 touch ../notebooks/all_exercises_challenges.html
-	 # Next scripts require a non-empty file at this point.
-	 echo "<br>" > ../notebooks/all_exercises_challenges.html
-    echo "Created empty all_exercises_challenges.html file."
-	 wait
-
-    # Add opening tags
-    source add_opening_tags.sh
-    wait
-    # Add title tag
-    source add_title.sh
-    wait
-    # Add css and js links, and favicon link
-    source add_css_js_links.sh
-    wait
-    # Close head and open body
-    source close_head_open_body.sh
-    wait
-    # Close body and html tags.
-    source close_body_html.sh
-    wait
-    # Add bootstrap.
-    source add_bootstrap.sh
-    wait
-
-	 # If custom index file exists, overwrite index just created.
-	 source copy_custom_index.sh
-	 wait
-
-	 # Customize all pages to use bootstrap
-     #  Also adds navbar from index to all pages
-	 printf "Customizing all styles to use bootstrap...\n"
-	 python add_bootstrap.py
-	 wait
-	 printf "Customized styles.\n\n"
-
-	 # Add elements to toggle output on each page.
-	 printf "Adding ability to toggle output on each page...\n"
-	 python show_hide_output.py
-	 wait
-	 printf "Added toggling ability.\n\n"
-
-	 # Convert index.html links to ./
-    source convert_home_links.sh
-    wait
-
-    # Add fb button.
-    source add_facebook.sh
-    wait
-    python modify_facebook_urls.py
-    wait
-
-	 # Convert image links from ipynb format to html.
-	 source convert_image_links.sh
-	 wait
-
-	 # Highlight lines of code.
-	 python highlight_code.py
-	 wait
-
-	 # Strip input references from code cells.
-	 python remove_input_references.py
-	 wait
-
-	 # Create page containing all exercises and challenges.
-	 python build_all_exercises_page.py
-	 wait
-
-    # Insert Google Analytics code.
-	 #  This should happen last, so that ga code is just before
-	 #  closing head tag.
-    source insert_google_analytics.sh
-	 wait
-
+	 # Probably running directly from /scripts directory.
+	 prefix=""
 fi
 
+
+# Build basic pages.
+source "$prefix"create_common_html.sh
+wait
+
+# Create empty all_exercises_challenges page.
+# Next scripts require a non-empty file at this point.
+echo "Creating empty all_exercises_challenges.html file..."
+if [ -e notebooks/ ]
+then
+	 touch notebooks/all_exercises_challenges.html
+	 echo "<br>" > notebooks/all_exercises_challenges.html
+else
+	 touch ../notebooks/all_exercises_challenges.html
+	 echo "<br>" > ../notebooks/all_exercises_challenges.html
+fi
+echo "Created empty all_exercises_challenges.html file."
+wait
+
+# Add opening tags
+source "$prefix"add_opening_tags.sh
+wait
+
+# Add title tag
+source "$prefix"add_title.sh
+wait
+
+# Add css and js links, and favicon link
+source "$prefix"add_css_js_links.sh
+wait
+
+# Close head and open body
+source "$prefix"close_head_open_body.sh
+wait
+
+# Close body and html tags.
+source "$prefix"close_body_html.sh
+wait
+
+# Add bootstrap.
+source "$prefix"add_bootstrap.sh
+wait
+
+# If custom index file exists, overwrite index just created.
+source "$prefix"copy_custom_index.sh
+wait
+
+# Customize all pages to use bootstrap
+#  Also adds navbar from index to all pages
+printf "Customizing all styles to use bootstrap...\n"
+python "$prefix"add_bootstrap.py
+wait
+printf "Customized styles.\n\n"
+
+# Add elements to toggle output on each page.
+printf "Adding ability to toggle output on each page...\n"
+python "$prefix"show_hide_output.py
+wait
+printf "Added toggling ability.\n\n"
+
+# Convert index.html links to ./
+source "$prefix"convert_home_links.sh
+wait
+
+# Add fb button.
+source "$prefix"add_facebook.sh
+wait
+python "$prefix"modify_facebook_urls.py
+wait
+
+# Convert image links from ipynb format to html.
+source "$prefix"convert_image_links.sh
+wait
+
+# Highlight lines of code.
+python "$prefix"highlight_code.py
+wait
+
+# Strip input references from code cells.
+python "$prefix"remove_input_references.py
+wait
+
+# Create page containing all exercises and challenges.
+python "$prefix"build_all_exercises_page.py
+wait
+
+# Insert Google Analytics code.
+#  This should happen last, so that ga code is just before
+#  closing head tag.
+source "$prefix"insert_google_analytics.sh
+wait
 
 printf "\n\n"
