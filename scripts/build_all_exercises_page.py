c53af414693db055214b897dfdd445049f76cf26
Eric Matthes <ehmatthes@gmail.com>
2013-12-24 05:02:31 -0900

    Scrapes Overall Challenges properly.

--- scripts/build_all_exercises_page.py
+++ scripts/build_all_exercises_page.py
@@ -117,7 +117,7 @@ for filename in filenames:
             current_h1_link = get_h1_link(filename, line)
             h1_label_linked = "<a href='%s'>%s</a>" % (current_h1_link, current_h1_label)
 
-        if '<h2 id="exercises' in line or '<h2 id="challenges' in line or '<h2 id="overall-challenges' in line:
+        if '<h2 id="exercises' in line or '<h2 id="challenges' in line or '<h1 id="overall-challenges' in line:
             # This is the signature of an exercise block.
             in_exercises_challenges = True
 
