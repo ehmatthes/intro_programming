9df5a3b7019f8bf71ba090e3629cf50bde7da0e3
Eric Matthes <ehmatthes@gmail.com>
2015-04-14 13:51:51 -0800

    Builds index with header, no body.

--- notebooks/my_templates/index.tpl
+++ notebooks/my_templates/index.tpl
@@ -1,2 +1,17 @@
-{%- extends 'intro_python_base.tpl' -%}
+{%- block header -%}
+	 {% include "intro_python_header.html" %}
+{%- endblock header -%}
+
+{% block body %}
+<body>
+  {% include "navbar.html" %}
+    <div class="container" id="notebook-container">
+    </div>
+</body>
+{%- endblock body %}
+
+{% block footer %}
+{% include "include_jquery.html" %}
+</html>
+{% endblock footer %}
 
--- scripts/build_index.py
+++ scripts/build_index.py
@@ -1,10 +1,17 @@
 from jinja2 import Environment, FileSystemLoader
 
+# Load index template.
 my_template_path = '/srv/projects/intro_programming/intro_programming/notebooks/my_templates'
 ipython_template_path = '/srv/projects/intro_programming/venv/lib/python3.4/site-packages/IPython/nbconvert/templates/html'
+
 my_loader = FileSystemLoader([my_template_path, ipython_template_path])
 env = Environment(loader=my_loader)
 
 index_template = my_loader.load(env, 'index.tpl')
-print(index_template)
-print(index_template.render())
+
+#print(index_template.render())
+# Render template to file.
+notebooks_path = '/srv/projects/intro_programming/intro_programming/notebooks/'
+filepath = notebooks_path + 'index.html'
+with open(filepath, 'w') as f:
+    f.write(index_template.render())
