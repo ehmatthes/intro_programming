d5ce67a143b4e7c6e25b1d583d51250bbf7bbd2f
Eric Matthes <ehmatthes@gmail.com>
2015-04-14 19:13:52 -0800

    Moved twitter buttons to ignored_templates.

--- scripts/build_index.py
+++ scripts/build_index.py
@@ -2,9 +2,11 @@ from jinja2 import Environment, FileSystemLoader
 
 # Load index template.
 my_template_path = '/srv/projects/intro_programming/intro_programming/notebooks/my_templates'
+my_template_base_path = '/srv/projects/intro_programming/intro_programming/notebooks'
 ipython_template_path = '/srv/projects/intro_programming/venv/lib/python3.4/site-packages/IPython/nbconvert/templates/html'
 
-my_loader = FileSystemLoader([my_template_path, ipython_template_path])
+my_loader = FileSystemLoader(
+    [my_template_path, my_template_base_path, ipython_template_path])
 env = Environment(loader=my_loader)
 
 index_template = my_loader.load(env, 'index.tpl')
