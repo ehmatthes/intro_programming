ed9082be48c70c755b5f68b7ed2924d4226ee23e
Eric Matthes <ehmatthes@gmail.com>
2017-05-25 09:05:45 -0800

    All notebooks have been moved to new directory structure.

--- build_html_site.py
+++ build_html_site.py
@@ -7,6 +7,7 @@ from subprocess import run
 print("Converting all ipynb files in notebooks/ to html.")
 print("cwd:", os.getcwd())
 
+# This should walk all dirs in notebooks/, find all ipynb files, then convert each.
 run(["python", "-m", "nbconvert", "notebooks/hello_world.ipynb", "--template=resources/my_templates/intro_python_base.tpl",
         "--FilesWriter.build_directory='html_site'"])
 
