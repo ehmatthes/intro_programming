6b6d9c357d6659c43f441a699690ae906e4b0bcc
Eric Matthes <ehmatthes@gmail.com>
2017-05-27 07:39:50 -0800

    Finds all ipynb files in notebooks/, recreates dir structure in html_site.

--- build_html_site.py
+++ build_html_site.py
@@ -1,13 +1,38 @@
 """Script to run conversion from ipynb to html."""
+# Find all ipynb files in notebooks/, then convert each.
+#  It should also preserve the directory structure found in notebooks/
 
-import os
+# DEV:
+#  Also need to copy resources such as images over to html_site?
+#   But not .py files.
+
+import os, sys
 
 from subprocess import run
+
 
 print("Converting all ipynb files in notebooks/ to html.")
 print("cwd:", os.getcwd())
 
-# This should walk all dirs in notebooks/, find all ipynb files, then convert each.
+new_dirs = []
+#pynb_files{path:filename}
+ipynb_files  = {}
+for root, dirs, files in os.walk("notebooks"):
+    for file in files:
+        if '.ipynb' in file:
+            ipynb_files[root] = file
+            
+            new_dir = root.replace('notebooks', 'html_site')
+            if new_dir not in new_dirs:
+                new_dirs.append(new_dir)
+
+# Create any directory in html_site, including root, that doesn't already exist.
+#  Should this clear out js and css from html_site, and all other dirs?
+for new_dir in new_dirs:
+    os.makedirs(new_dir, exist_ok=True)
+
+sys.exit()
+
 
 
 # Need to run any pre-processing on raw .ipynb files?
