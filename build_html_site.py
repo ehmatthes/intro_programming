315cc54d21597b26dbfadf63dc7dabdf7295476e
Eric Matthes <ehmatthes@gmail.com>
2017-05-29 09:38:53 -0800

    Copies js and css resources to html_site.

--- build_html_site.py
+++ build_html_site.py
@@ -2,14 +2,12 @@
 # Find all ipynb files in notebooks/, then convert each.
 #  It should also preserve the directory structure found in notebooks/
 
-# DEV:
-#  Also need to copy resources such as images over to html_site?
-#   But not .py files.
-
 import os, sys
+import shutil
+from distutils import dir_util
 
 from subprocess import run
-from shutil import rmtree
+
 
 
 print("Converting all ipynb files in notebooks/ to html.")
@@ -27,10 +25,10 @@ for root, dirs, files in os.walk("notebooks"):
             if new_dir not in new_dirs:
                 new_dirs.append(new_dir)
 
-# Delete html_site, or empty it. Copy js and css resources to the directory.
+# Delete existing html_site.
 print("\nDeleting current html_site directory...")
 try:
-    rmtree('html_site')
+    shutil.rmtree('html_site')
     print("  Deleted html_site.")
 except FileNotFoundError:
     print("  No html_site directory found.")
@@ -41,6 +39,16 @@ for new_dir in new_dirs:
     os.makedirs(new_dir, exist_ok=True)
 print("  Built html_site directory.")
 
+# Copy js and css resources into html_site.
+#  Note this is distutils.dir_util.copy_tree, not shutils.copytree.
+#  The shutils version fails if parent directory exists.
+print("\nCopying js and css resources into html_site...")
+dir_util.copy_tree('resources/js', 'html_site/js')
+dir_util.copy_tree('resources/css', 'html_site/css')
+print("  Created html_site, and copied js and css resources.")
+
+
+
 sys.exit()
 
 
@@ -48,6 +56,8 @@ sys.exit()
 # Need to run any pre-processing on raw .ipynb files?
 #  ie, respond to any cell metadata or tags?
 #  can remove cells that are tagged "invisible"
+
+# DEV: may need to copy images and some other resources, but not .py files.
 
 # This gets the command used to run this script. If the user calls with python,
 #  it uses that command, if they use python3.6, it uses that command.
